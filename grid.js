879d509ac3e4059b388a0de3a776218b09a06e7b
hassehulabeck <hansbudandersson@gmail.com>
2017-09-27 14:41:07 +0200

    Nu med 'styrninga la knappsats

--- grid.js
+++ grid.js
@@ -18,13 +18,29 @@ function getPosition() {
     }
 }
 
-function moveIt(x,y) {
+function moveIt(riktning) {
     // Hitta nuvarande position. Resultaten blir en array.
     let position = getPosition();
+    x = position[0];
+    y = position[1];
     
     // Töm den ruta där x fanns.
-    grid[position[0]][position[1]] = ' ';
+    grid[x][y] = ' ';
     
+    switch (riktning) {
+        case 1: x--; y--; break;
+        case 2: x--; break;
+        case 3: x--; y++; break;
+        case 4: y--; break;
+        case 6: y++; break;
+        case 7: x++; y--; break;
+        case 8: x++;  break;
+        case 9: x++; y++; break;
+    }
+    
+    x = (x < 0 ? 2 : x);
+    y = (y < 0 ? 2 : y);
+
     // Placera x i den önskade positionen.
     grid[x][y] = 'x';
     
@@ -38,5 +54,9 @@ function printGrid() {
 }
 
 printGrid();
-moveIt(2,2);
+moveIt(2); // Flytta på den enligt en knappsats, dvs 2 = uppåt.
+printGrid();
+moveIt(6); // Flytta på den enligt en knappsats, dvs 2 = uppåt.
+printGrid();
+moveIt(7); // Flytta på den enligt en knappsats, dvs 2 = uppåt.
 printGrid();
